c406c4d3b285cbae4232457c705fd8ecb94cc04e
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-04-22 23:12:53 +0800

    changing baseURl

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Header from './common/Header';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 //set base url 
-axios.defaults.baseURL='http://127.0.0.1:8000/api';
+//axios.defaults.baseURL='http://127.0.0.1:8000/api';
+axios.defaults.baseURL='http://petcare.c1.is/api';
 //get the token and save for authorization to be used by api user route
 axios.defaults.headers.common['Authorization']='Bearer ' + localStorage.getItem('token');
 const root = ReactDOM.createRoot(document.getElementById('root'));
